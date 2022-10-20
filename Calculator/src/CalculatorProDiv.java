335e285b7ea4a1a8c9b3f5954dc05c82e7070ff3
Pavithran2612 <pavithran2626rkvp@gmail.com>
2022-10-20 15:09:56 +0530

    Added Product and Division methods

---
+++ Calculator/src/CalculatorProDiv.java
@@ -0,0 +1,16 @@
+
+public class CalculatorProDiv {
+	
+	//Product method
+	public void product(double num1, double num2) {
+		double proResult = num1 * num2;
+		System.out.print("\nProduct of Two values are: " + proResult);
+	}
+	
+	//Division method
+	public void divide(double num1, double num2) {
+		double divResult = num1 / num2;
+		System.out.print("\nDivision of Two values are: " + divResult);
+	}
+
+}
