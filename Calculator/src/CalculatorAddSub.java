1b5d0316395674f144369c5ac56fc49e3990c513
Salman-003 <dsalman62@gmail.com>
2022-10-20 14:32:08 +0530

    Addition & Subtraction function added

---
+++ Calculator/src/CalculatorAddSub.java
@@ -0,0 +1,16 @@
+
+public class CalculatorAddSub {
+	
+	//Addition method
+	public void add(double num1, double num2) {
+		double addResult = num1 + num2;
+		System.out.println("Addition result: " + addResult);
+	}
+	
+	//subtraction method
+	public void sub(double num1, double num2) {
+		double subResult = num1 - num2;
+		System.out.println("Subtraction result: " + subResult);
+	}
+
+}
